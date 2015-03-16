a9957df423627120e31d4307165181b30c608f9b
rulerstorm <rocklule@163.com>
2015-03-16 16:00:23 +0800

    init

---
+++ MiXi/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  mixi
+//
+//  Created by LuRock on 15/3/15.
+//  Copyright (c) 2015年 LuRock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+
+}
+
---
+++ RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  mixi
+//
+//  Created by LuRock on 15/3/15.
+//  Copyright (c) 2015年 LuRock. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        self.navigationItem.title = "注册界面"
+    }
+
+
+}
